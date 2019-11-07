5b409f331eeaf7b24a992c72377c7bee5d3cb1c1
Marco Antonio <marcoantoniojt@gmail.com>
2019-11-07 09:57:23 -0300

    First

---
+++ src/projeto/tc/ManipuladorArquivo.java
@@ -0,0 +1,11 @@
+
+package projeto.tc;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import.java.io.FileReader;
+import.java.io.FileWriter
+
+public class ManipuladorArquivo {
+    
+}
---
+++ src/projeto/tc/ProjetoTC.java
@@ -0,0 +1,59 @@
+
+package projeto.tc;
+
+public class ProjetoTC {
+
+    public static void main(String[] args) {
+        String x  = null;
+        String y = null;
+        
+        switch(x){
+            case " ":{
+                y = "a0000";
+                break;
+            }
+            case "0":{
+                y = "a0001";
+                break;
+            }
+            case "1":{
+                y = "a0010";
+                break;
+            }
+            case "2":{
+                y = "a0011";
+                break;
+            }
+            case "3":{
+                y = "a0100";
+                break;
+            }
+            case "4":{
+                y = "a0101";
+                break;
+            }
+            case "5":{
+                y = "a0110";
+                break;
+            }
+            case "6":{
+                y = "a0111";
+                break;
+            }
+            case "7":{
+                y = "a1000";
+                break;
+            }
+            case "8":{
+                y = "a1001";
+                break;
+            }
+            case "9":{
+                y = "a1010";
+                break;
+            }
+        }
+        
+    }
+    
+}
